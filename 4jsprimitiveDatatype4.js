19cf905d5bfebc0d0492742f3adaebef533a4597
Hrushiprada Swain <hrushipradaswain@gmail.com>
2025-06-19 21:51:58 +0530

    Add files via upload

---
+++ 4jsprimitiveDatatype4.js
@@ -0,0 +1,41 @@
+let name="Hruhi";
+let ageE=22;
+let isStudent=true;
+let car=null;
+let job;
+let age=34;
+let uniqueid=Symbol("id");
+let bigNumber=123456789512434244244252n;
+let d=BigInt("567")+BigInt("3");
+
+//objects in JS
+const item={
+    "Harry":true,
+    "h1":23,
+    "l2":undefined,
+    "h5":false
+}
+let a=7;
+let b="Hrushi";
+console.log(a+b);
+//check the datatype
+console.log(typeof(a));
+console.log(typeof(b));
+
+const dict={
+    h1:"Hello javascript",
+    h2:"Hi java",
+    h3:"How have you been?"
+}
+console.log(dict);console.log(dict.h1);console.log(dict.h2);console.log(dict.h3);
+
+console.log(item["h1"]);
+console.log("Name is:",name);
+console.log("Age is:",ageE);
+console.log("if you are a student:",isStudent);
+console.log("if you have a car:",car);
+console.log("your job is:",job);
+console.log("your uniqueue id  is:",uniqueid);
+console.log("bignumber  is:",bigNumber);
+console.log("The new age is:",age);
+console.log("the sum of BigInteger value is:",d);
