e4911ab4f44f3b6c6236d7ab2b36fb8238c63e6d
Hrushiprada Swain <hrushipradaswain@gmail.com>
2025-06-20 20:14:31 +0530

    Add files via upload

---
+++ 5OperateorInjs5.js
@@ -0,0 +1,31 @@
+//Arithmetic Oprator
+let a=56;
+let b=34;
+console.log("Add two number is:",a+b);
+console.log("substract two number is:",a-b);
+console.log("Multiplication two number is:",a*b);
+console.log("Division two number is:",a/b);
+console.log("Modulus of two number is:",a%b);
+console.log("Power of number is:",a**b);
+console.log("Increment of 'a' number is:",++a);
+console.log("Decrement of 'a' number is:",--a);
+
+//comparision operator
+let m=56.04;
+let n=90.23;
+let c=m+n;
+let d=0.1;
+let e=0.2;
+console.log("m>n=",m>n);
+console.log("m===n:",m===n);
+console.log("m<n:",m<n);
+console.log("m!=n:",m!=n);
+console.log("The value of m+n is:",c);
+console.log("comparision:m+n value is:",146.270===c);
+console.log(0.3===(d+e));
+
+//logical operator
+let r=45;
+let s=56;
+console.log(r<s && r!=s);
+console.log(r<s || r==s);
