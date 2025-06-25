3c5cf4d09f53053f53451e4ccbc9b5ef3e87591c
Hrushiprada Swain <hrushipradaswain@gmail.com>
2025-06-25 23:29:31 +0530

    Add files via upload

---
+++ 7LoopingJs7.js
@@ -0,0 +1,60 @@
+/* //sum of natural number
+let i,sum=0;
+for(i=0;i<=100;i++){
+    sum=sum+i;
+    //console.log(sum);
+}
+console.log("The sum of natural number is from 1 to 100:",sum);
+
+//find Factorial
+let j,fact=1;
+for(j=1;j<=10;j++){
+    fact*=i;
+}
+console.log("Factorial of number is:",fact);
+ */
+
+//For-In loop
+/* let obj={
+    harry:45,
+    subha:56,
+    Gudulu:78,
+    kanha:98,
+    Luli:34
+}
+for(let a in obj){
+    console.log("Marks of "+a+" are-:"+obj[a]);
+} */
+/* 
+let obj1={
+    harry:45,
+    subha:56,
+    Gudulu:78,
+    kanha:98,
+    Luli:34
+}
+for(let b of "harry"){
+    console.log(b);
+}
+for(let c of "kanha"){
+    console.log(c);
+} */
+
+//While Loop
+
+/* let i=0;
+while(i<12){
+    console.log(i);
+    i++;
+} */
+
+//do-while Loop
+
+/* let i=1;
+do{
+    console.log(i);
+    i++
+}
+while(i<23) */
+
+
