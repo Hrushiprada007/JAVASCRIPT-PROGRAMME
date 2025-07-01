e44d13ad80955b6451a204849a5d9ac017c30a61
Hrushiprada Swain <hrushipradaswain@gmail.com>
2025-07-01 18:53:03 +0530

    Add files via upload

---
+++ 9StringsJS.js
@@ -0,0 +1,67 @@
+/* let name="Hrushi";
+console.log(name);
+console.log(name.length);
+let frnd='Bikash';
+console.log(frnd);
+console.log(frnd[3]);
+console.log(frnd[2]); */
+
+//Template Literals
+
+/* let boy1="Halumn";
+let boy2="Nakayama";
+//print Halumn is the friend of Nakayama
+
+let sentence=`${boy1} is the friend of ${boy2}`
+console.log(sentence); */
+
+//Escape Sequence characters
+
+/* let name='Adam D\'Angelo'
+console.log(name);
+let msg="hello m\"hass"
+console.log(msg);
+
+let name1="Hrushi";
+console.log(name1);
+let name2="hrushi";
+console.log(name2.toUpperCase())
+let name3="HRUSHI s\"YT channel"
+console.log(name3.toLowerCase());
+console.log(name1.length);
+
+//slicing 
+let name5="Hrushi"
+console.log(name5.slice(2,4))
+console.log(name5.slice(3))
+//Replace
+let name6="Hrushi"
+let newName=name6.replace("Hrushi","Java Developer")
+console.log(newName) */
+/* 
+let name1="Hrushi"
+let name2="Yamaguchhi"
+let name3=name1.concat(" is a friend of ",name2," 'yes'")
+console.log(name3);
+
+let name4="          Hrushi         ";
+console.log(name4);
+console.log(name4.trim()); */
+
+
+//Pratice set question
+
+console.log("har\"".length);
+
+let a="heiyamaguchhi";
+console.log(a.toUpperCase());
+let b1="HERYEE";
+console.log(b1.toLowerCase());
+
+let str2="please give me rupees 7000";
+let amount=str2.slice("please give me rupees".length+1);
+console.log(amount);
+
+let f="KeiYamaguchhi";
+let f1=f.replace(f[4],f[6]);
+console.log(f1);//string not to be changed because string is immutatble
