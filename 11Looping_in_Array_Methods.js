c9c060a0b51c15283859c7204dae7eb11079f90e
Hrushiprada Swain <hrushipradaswain@gmail.com>
2025-07-03 22:29:11 +0530

    Add files via upload

---
+++ 11Looping_in_Array_Methods.js
@@ -0,0 +1,39 @@
+//ForEach Loop 
+/* for(let j=0;j<=12;j++){
+    if(j==6){
+    break;
+    }
+    console.log(j);
+}
+
+for(let m=0;m<=15;m++){
+    if(m==12){
+        continue;
+    }
+    console.log(m);
+} */
+
+//Array.from
+/* let name="Hrushi";
+let name1="Jaguar";
+let arr=Array.from(name);
+let arr1=Array.from(name1);
+console.log(arr1);
+console.log(arr);
+let n3=Array.from("Hello Hrushi");
+console.log(n3); */
+
+//for-of loop
+/* let k=[4,5,6,7,8,2,3];
+for(let i of k){
+    console.log(i);
+   
+}
+console.log(k.reverse()); */
+//for-in Loop
+let l=[4,5,6,7,8,2,3];
+for(let j in l){
+    console.log(j);
+    //console.log(num[j]);
+
+}
